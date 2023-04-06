c449a58835c87953e3afcf3d2a5300264d0b734a
HaramiTV <122385162+HaramiTV@users.noreply.github.com>
2023-04-06 20:56:56 +0300

    Double debug (#10)
    * Update method is changed
    
    Added debug.log to update method.
    
    * HACKED
    
    hacked
    
    * Revert "HACKED"
    
    This reverts commit 01a171bc5882e2b47d3229d6caf653d6a81c3d4d.

--- LD53/Assets/Code/test.cs
+++ LD53/Assets/Code/test.cs
@@ -13,6 +13,7 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("UPDATEEEEEEEEE!!!");
         Debug.Log("gsgdusdsfghdgfhdgfhdjghf");
     }
 }
