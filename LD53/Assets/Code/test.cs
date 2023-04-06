a7e21735ae9e0d8769e0b477b589b4cc82d3b83d
GraphicEdit <contact@graphicedit.com>
2023-04-06 18:12:10 +0100

    this is

--- LD53/Assets/Code/test.cs
+++ LD53/Assets/Code/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Testttttttttt...");
+        Debug.Log("TesttttttttttAAAAAAAA...");
     }
 
     // Update is called once per frame
