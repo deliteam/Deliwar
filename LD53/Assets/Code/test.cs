a56d870cfbd2570822fe57d6ca8c6f25baa094e9
GraphicEdit <contact@graphicedit.com>
2023-04-06 18:28:40 +0100

    test x

--- LD53/Assets/Code/test.cs
+++ LD53/Assets/Code/test.cs
@@ -13,6 +13,6 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("UPDATEEEEEEEEE!!!");
+        Debug.Log("gsgdusdsfghdgfhdgfhdjghf");
     }
 }
