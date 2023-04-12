686cf9261506b151b944274482639e20582edfaa
GraphicEdit <contact@graphicedit.com>
2023-04-12 22:00:10 +0100

    BacktoMenu

---
+++ LD53/Assets/Code/BacktoMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BacktoMenu : MonoBehaviour
+{
+   
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu_Start");
+    }
+ }
