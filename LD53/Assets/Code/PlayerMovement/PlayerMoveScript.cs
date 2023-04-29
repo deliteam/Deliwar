c0c19343d1e9b783d8a3745e7826c4cecc3d7b51
DESKTOP-U5GJ455\fatih <fatihkavum@gmail.com>
2023-04-29 14:03:25 +0300

    fix

--- LD53/Assets/Code/PlayerMovement/PlayerMoveScript.cs
+++ LD53/Assets/Code/PlayerMovement/PlayerMoveScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
@@ -7,40 +8,47 @@ public class PlayerMoveScript : MonoBehaviour
     public float jumpForce = 10f; // zıplama kuvveti
     float horizontalInput;
     bool jumpInput;
+
     public bool isGrounded = false; // yerde mi değil mi kontrolü
+
     // diğer değişkenler buraya gelecek
     void Update()
-{
-    horizontalInput = Input.GetAxis("Horizontal"); // yatay girdi (A ve D tuşları veya sol ve sağ oklar)
-    jumpInput = Input.GetKey(KeyCode.Space); // zıplama girdisi (boşluk tuşu)
-    // diğer kodlar buraya gelecek
-}
-void FixedUpdate()
-{
-    // yatay hareket için MovePosition kullanımı
-    Vector2 position = rb.position; // mevcut pozisyon
-    position.x += horizontalInput * speed * Time.fixedDeltaTime; // yatay girdiye göre pozisyonu güncelleme
-    rb.MovePosition(position); // rigidbody'nin pozisyonunu ayarlama
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); // yatay girdi (A ve D tuşları veya sol ve sağ oklar)
+        jumpInput = Input.GetKeyDown(KeyCode.Space); // zıplama girdisi (boşluk tuşu)
+        // diğer kodlar buraya gelecek
+    
+     // zıplama için AddForce kullanımı
+            
+       if (jumpInput && isGrounded) // zıplama girdisi varsa ve yerdeyse
+       {
+           rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // rigidbody'ye yukarı yönde bir kuvvet uygula
+       }
+    }
 
-    // zıplama için AddForce kullanımı
-    if (jumpInput && isGrounded) // zıplama girdisi varsa ve yerdeyse
+    void FixedUpdate()
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // rigidbody'ye yukarı yönde bir kuvvet uygula
-    }
-}
-void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Ground")) // diğer collider yer etiketine sahipse
-    {
-        isGrounded = true; // yerde olduğunu belirle
-    }
-}
+        // yatay hareket için MovePosition kullanımı
+        Vector2 position = rb.velocity; // mevcut pozisyon
+        position.x = horizontalInput * speed; // yatay girdiye göre pozisyonu güncelleme
+        rb.velocity = position; 
 
-void OnTriggerExit2D(Collider2D other)
-{
-    if (other.CompareTag("Ground")) // diğer collider yer etiketine sahipse
-    {
-        isGrounded = false; // yerde olmadığını belirle
+   
     }
-}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground")) // diğer collider yer etiketine sahipse
+        {
+            isGrounded = true; // yerde olduğunu belirle
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground")) // diğer collider yer etiketine sahipse
+        {
+            isGrounded = false; // yerde olmadığını belirle
+        }
+    }
 }
