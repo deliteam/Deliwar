4763581f75d603638f900d7d92c70c47b9593cac
HaramiTV <122385162+HaramiTV@users.noreply.github.com>
2023-04-29 20:16:55 +0300

    Fix: #50 - Fixed EnemyPatrolling.cs & Added PatrollingEnemyDog.prefab
    EnemyPatrolling.cs: Needs an array of two waypoints to work as shown on sample prefab (PatrollingEnemyDog.prefab).

--- LD53/Assets/Code/EnemyDog/EnemyPatrolling.cs
+++ LD53/Assets/Code/EnemyDog/EnemyPatrolling.cs
@@ -7,42 +7,39 @@ public class EnemyPatrolling : MonoBehaviour
 {
     public float speed = 2f;
     public Transform[] waypoints;
-    public bool isMovingRight = true;
+    public bool isMovingLeft = false;
 
     private int currentWaypointIndex = 0;
 
-    private float startPos;
-    private void Awake() {
-        startPos = this.transform.position.x;
-    }
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, speed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
         {
-            if (isMovingRight)
+            if (isMovingLeft)
             {
-
                 currentWaypointIndex++;
+
                 if (currentWaypointIndex >= waypoints.Length)
                 {
                     currentWaypointIndex = waypoints.Length - 2;
-                    isMovingRight = false;
+                    isMovingLeft = false;
                 }
             }
             else 
             {
                 currentWaypointIndex--;
+
                 if (currentWaypointIndex < 0)
                 {
                     currentWaypointIndex = 1;
-                    isMovingRight = true;
+                    isMovingLeft = true;
                 }
             }
         }
 
-        if (isMovingRight)
+        if (!isMovingLeft)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
