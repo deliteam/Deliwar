fc7baea1ffbeae7a353185e14e8de766b9f3bef3
Mert Çınar <kadirmertcnr@gmail.com>
2023-04-30 14:10:37 +0300

    MainMenu renewed & Volume slider added & Menu music can be controlled

---
+++ LD53/Assets/Code/OptionsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume){
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+}
