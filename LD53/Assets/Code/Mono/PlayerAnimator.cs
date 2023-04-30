c945d68aace8626b8c1fb87a8962c03ec67a10d1
DESKTOP-U5GJ455\fatih <fatihkavum@gmail.com>
2023-04-30 22:08:17 +0300

    Player Animation fixes #2

---
+++ LD53/Assets/Code/Mono/PlayerAnimator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Code
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        private const string DamagedParam = "Damaged";
+        private const string MeleeAttackParam = "MeleeAttack";
+        private const string RunParam = "Run";
+        private const string WalkParam = "Walk";
+        private const string IdleParam = "Idle";
+        private const string JumpParam = "Jump";
+        
+        private static readonly int DamagedParamId = Animator.StringToHash(DamagedParam);
+        private static readonly int MeleeAttackParamId = Animator.StringToHash(MeleeAttackParam);
+        private static readonly int RunParamId = Animator.StringToHash(RunParam);
+        private static readonly int WalkParamId = Animator.StringToHash(WalkParam);
+        private static readonly int IdleParamId = Animator.StringToHash(IdleParam);
+        private static readonly int JumpParamId = Animator.StringToHash(JumpParam);
+
+        [SerializeField] private Animator[] _animators;
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                SetDamagedAnim();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                SetIdleAnim();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                SetJumpAnim();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                SetRunAnim();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                SetWalkAnim();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                SetMeleeAttackAnim();
+            }
+        }
+
+        public void SetDamagedAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,true);
+                animator.SetBool(MeleeAttackParamId,false);
+                animator.SetBool(RunParamId,false);
+                animator.SetBool(WalkParamId,false);
+                animator.SetBool(IdleParamId,false);
+                animator.SetBool(JumpParamId,false);
+            }
+        }
+        
+        public void SetMeleeAttackAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,false);
+                animator.SetBool(MeleeAttackParamId,true);
+                animator.SetBool(RunParamId,false);
+                animator.SetBool(WalkParamId,false);
+                animator.SetBool(IdleParamId,false);
+                animator.SetBool(JumpParamId,false);
+            }
+        }
+        
+        public void SetRunAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,false);
+                animator.SetBool(MeleeAttackParamId,false);
+                animator.SetBool(RunParamId,true);
+                animator.SetBool(WalkParamId,false);
+                animator.SetBool(IdleParamId,false);
+                animator.SetBool(JumpParamId,false);
+            }
+        }
+        
+        public void SetWalkAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,false);
+                animator.SetBool(MeleeAttackParamId,false);
+                animator.SetBool(RunParamId,false);
+                animator.SetBool(WalkParamId,true);
+                animator.SetBool(IdleParamId,false);
+                animator.SetBool(JumpParamId,false);
+            }
+        }
+        
+        public void SetIdleAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,false);
+                animator.SetBool(MeleeAttackParamId,false);
+                animator.SetBool(RunParamId,false);
+                animator.SetBool(WalkParamId,false);
+                animator.SetBool(IdleParamId,true);
+                animator.SetBool(JumpParamId,false);
+            }
+        }
+        
+        public void SetJumpAnim()
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetBool(DamagedParamId,false);
+                animator.SetBool(MeleeAttackParamId,false);
+                animator.SetBool(RunParamId,false);
+                animator.SetBool(WalkParamId,false);
+                animator.SetBool(IdleParamId,false);
+                animator.SetBool(JumpParamId,true);
+            }
+        }
+    }
+}
--- LD53/Assets/Code/PlayerScript.cs
+++ LD53/Assets/Code/PlayerScript.cs
@@ -8,10 +8,12 @@ public enum PlayerMoveState
     Walk,
     Jump
 }
-public class PlayerMoveScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private PlayerAnimator _playerAnimator;
+    [SerializeField] private PlayerPackageController _packageContainer;
+    
     [SerializeField] private PlayerMoveState _playerMoveState;
-    public Animator[] Animators;
     public Rigidbody2D rb; 
     public float speed = 5f; 
     public float jumpForce = 10f; 
@@ -21,44 +23,47 @@ public class PlayerMoveScript : MonoBehaviour
 
     public bool isGrounded = false;
 
-    void Update()
+    private void Update()
     {
         CheckGround();
-        horizontalInput = Input.GetAxis("Horizontal"); // yatay girdi (A ve D tuşları veya sol ve sağ oklar)
-        jumpInput = Input.GetKeyDown(KeyCode.Space); // zıplama girdisi (boşluk tuşu)
-    
-            
-       if (jumpInput && isGrounded) 
-       {
-           rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);  
-       }
-
-       if (!isGrounded && _playerMoveState != PlayerMoveState.Jump)
-       {
-           _playerMoveState = PlayerMoveState.Jump;
-           foreach (var animator in Animators)
-           {
-               animator.SetTrigger("Jump");
-           }
-       }
-       else if (horizontalInput == 0 && _playerMoveState != PlayerMoveState.Idle)
-       {
-           _playerMoveState = PlayerMoveState.Idle;
-           foreach (var animator in Animators)
-           {
-               animator.SetTrigger("Idle");
-           }
-       }
-       else if(horizontalInput != 0 && _playerMoveState != PlayerMoveState.Walk)
-       {
-           _playerMoveState = PlayerMoveState.Walk;
-           foreach (var animator in Animators)
-           {
-               animator.SetTrigger("Walk");
-           }
-       }
+        SetInputParams();
+        CheckJumpAction();
+        CheckState();
     }
-    
+
+    private void CheckState()
+    {
+        if (!isGrounded && _playerMoveState != PlayerMoveState.Jump)
+        {
+            _playerMoveState = PlayerMoveState.Jump;
+            _playerAnimator.SetJumpAnim();
+        }
+        else if (horizontalInput == 0 && _playerMoveState != PlayerMoveState.Idle && isGrounded)
+        {
+            _playerMoveState = PlayerMoveState.Idle;
+            _playerAnimator.SetIdleAnim();
+        }
+        else if (horizontalInput != 0 && _playerMoveState != PlayerMoveState.Walk && isGrounded)
+        {
+            _playerMoveState = PlayerMoveState.Walk;
+            _playerAnimator.SetWalkAnim();
+        }
+    }
+
+    private void CheckJumpAction()
+    {
+        if (jumpInput && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void SetInputParams()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
